39b434ed90b1ac752fc60c6f0fee582d0590da31
Keveen Menezes <keveen.menezes@pjus.com.br>
2023-04-13 19:03:50 -0300

    Salvar sidebar

--- Layout/Layout.js
+++ Layout/Layout.js
@@ -3,7 +3,17 @@
     'use strict'
     const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     tooltipTriggerList.forEach(tooltipTriggerEl => {
-      new bootstrap.Tooltip(tooltipTriggerEl)
+        new bootstrap.Tooltip(tooltipTriggerEl)
     })
-  })()
-  
+})()
+
+function openSide() {
+    const sidebar = document.getElementById('mySidebar');
+    sidebar.classList.add('active');
+}
+
+function closedSide() {
+    const sidebar = document.getElementById('mySidebar');
+    sidebar.classList.remove('active');
+}
+
