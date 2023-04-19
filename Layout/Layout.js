0c460101094279c8e08d989e225f39ffd42b6cae
Keveen Menezes <keveen.menezes@pjus.com.br>
2023-04-19 19:31:26 -0300

    confimar

--- Layout/Layout.js
+++ Layout/Layout.js
@@ -7,13 +7,43 @@
     })
 })()
 
+/* interação sidebar*/
 function openSide() {
   const sidebar = document.getElementById('mySidebar');
+  const perfilSideBar = document.getElementById('perfilSideBar');
   sidebar.classList.add('active');
+  perfilSideBar.classList.add('dropdown-toggle');
 }
 
 function closedSide() {
   const sidebar = document.getElementById('mySidebar');
+  const perfilSideBar = document.getElementById('perfilSideBar');
   sidebar.classList.remove('active');
+  perfilSideBar.classList.remove('dropdown-toggle')
 }
 
+/*botão de pesquisa */
+const searchInputDropdown = document.getElementById('navbarDropdownMenuLink');
+const dropdownOptions = document.getElementById('aqui');
+
+searchInputDropdown.addEventListener('input', () => {
+  const filter = searchInputDropdown.value.toLowerCase();
+  showOptions();
+  const valueExist = !!filter.length;
+
+  if (valueExist) {
+    dropdownOptions.forEach((el) => {
+      const elText = el.textContent.trim().toLowerCase();
+      const isIncluded = elText.includes(filter);
+      if (!isIncluded) {
+        el.style.display = 'none';
+      }
+    });
+  }
+});
+
+const showOptions = () => {
+  dropdownOptions.forEach((el) => {
+    el.style.display = 'flex';
+  })
+}
